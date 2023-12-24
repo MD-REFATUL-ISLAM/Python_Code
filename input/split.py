1e8f543163de9405cd510e05a788973a89fe3a79
MD-REFATUL-ISLAM <146784571+MD-REFATUL-ISLAM@users.noreply.github.com>
2023-12-24 18:56:29 +0000

    save

---
+++ input/split.py
@@ -0,0 +1,9 @@
+# taking multiple input at a time
+
+x,y,z = input("Enter three value : ").split(',')
+
+print("number of boys : ", x)
+print("number of girls : ", y)
+print("number of teacher :", z)
+
+print("the first numer is {} and second number is {}".format(x,y))
