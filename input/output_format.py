f6d51f69b1895511edf5889a7c86a9d364f0f094
MD-REFATUL-ISLAM <146784571+MD-REFATUL-ISLAM@users.noreply.github.com>
2023-12-24 20:44:15 +0000

    save

--- input/output_format.py
+++ input/output_format.py
@@ -4,26 +4,22 @@ age = 30
 salary = 4903.89
 
 print(" Name:%s \n Age:%d \n Salary:%f" %(name,age,salary))
+# Name:Md Refatul Islam 
+# Age:30 
+# Salary:4903.890000
 print("Name:{} \nAge:{} \nSalary:{}".format(name,age,salary))
+# Name:Md Refatul Islam 
+# Age:30 
+# Salary:4903.89
+
 print("Name:{0} Age:{1} Salary:{2}".format(name,age,salary))
 print("Name:{0} Age:{0} Salary:{0}".format(name,age,salary))
 print("Name:{1} Age:{1} Salary:{1}".format(name,age,salary))
 print("Name:{2} Age:{2} Salary:{2}".format(name,age,salary))
 
 """
- Name:Md Refatul Islam 
- Age:30 
- Salary:4903.890000
-
-Name:Md Refatul Islam 
-Age:30 
-Salary:4903.89
-
 Name:Md Refatul Islam Age:30 Salary:4903.89
-
 Name:Md Refatul Islam Age:Md Refatul Islam Salary:Md Refatul Islam
-
 Name:30 Age:30 Salary:30
-
 Name:4903.89 Age:4903.89 Salary:4903.89
 """
