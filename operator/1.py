8088c0d71d44a08d01fd7eff785838a6108e2ab5
MD-REFATUL-ISLAM <146784571+MD-REFATUL-ISLAM@users.noreply.github.com>
2023-12-25 14:31:33 +0000

    save

--- operator/1.py
+++ operator/1.py
@@ -1,9 +1,30 @@
+
+# Arithmatic operators
 x = 10
 y = 3
 
 print(x+y)
+# Addition: adds two operands
 print(x-y)
+# Subtraction: subtracts two operands
 print(x*y)
+# Multiplication: multiplies two operands
 print(x/y)
+# Division (float): divides the first operand by the second
 print(x//y)
+# Division (floor): divides the first operand by the second
 print(x%y)
+# Modulus: returns the remainder when the first operand is divided by the second
+print(x**y)
+# Power (Exponent): Returns first raised to power second
+
+"""
+@MD-REFATUL-ISLAM ➜ /workspaces/Python_Code/operator (main) $ python 1.py
+13
+7
+30
+3.3333333333333335
+3
+1
+1000
+"""
---
+++ operator/2.py
@@ -0,0 +1,22 @@
+
+a, b = 10, 20
+
+if a != b:
+
+    if a < b:
+
+         print(True)
+
+    else:
+
+        print(False)
+
+else : 
+
+    print("both are equal")
+    
+
+
+
+
+
