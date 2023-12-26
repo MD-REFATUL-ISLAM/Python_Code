3ce3d0697b4b8c6936e8dce603524986607ecdb7
MD-REFATUL-ISLAM <146784571+MD-REFATUL-ISLAM@users.noreply.github.com>
2023-12-26 02:01:29 +0000

    save

--- function/6.py
+++ function/6.py
@@ -1,4 +1,10 @@
 
+
+# Keyword Arguments : The idea is to allow the caller
+# to specify the argument name with values so that
+# the caller does not need to remember the order of parameters.
+
+
 def student(firstname,lastname):
     print(firstname,lastname)
 
