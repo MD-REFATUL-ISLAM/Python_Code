f525f9562eeb927a2e4466eaea8809e93e463850
MD-REFATUL-ISLAM <146784571+MD-REFATUL-ISLAM@users.noreply.github.com>
2023-12-27 15:52:25 +0000

    save

--- conditional/while.py
+++ conditional/while.py
@@ -1,6 +1,13 @@
 
-
+# increamental 
 pound = 0
-while (pound < 7) :
+while (pound < 5) :
     pound = pound + 1
     print("orchie")
+
+
+# decremental
+count = 5
+while (count > 0):
+    count = count - 1
+    print("hello")
