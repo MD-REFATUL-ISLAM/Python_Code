9892f0f47e771e114cb3f870d61d3114604179cf
MD-REFATUL-ISLAM <146784571+MD-REFATUL-ISLAM@users.noreply.github.com>
2023-12-27 14:35:43 +0000

    save

--- operator/2.py
+++ operator/2.py
@@ -1,14 +1,14 @@
 
-a, b = 10, 20
+a, b =[int(a) for a in input("enter two value : ").split(",")]
 
 if a != b:
 
     if a < b:
-
+         print(f"{a} is less than {b}")
          print(True)
 
     else:
-
+        print(f"{a} is greater than {b}")
         print(False)
 
 else : 
